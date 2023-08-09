1342dda1f0c8bab6f1e7407bce3148a8bee4d9e9
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2023-08-10 00:18:12 +0530

    design skeleton signup done

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,15 +8,36 @@ export default function loginPage() {
           <p className="text-3xl text-blue-600 font-bold">Sign in to App</p>
           <form className="w-96 mt-6">
             {/* TODO make input component */}
-            <div className="my-4">
+            <div className="h-24">
               <label
                 htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white invalid:text-red-500"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
               >
-                user name
+                E-mail
               </label>
               <input
                 type="email"
+                className="peer mt-1 block w-full px-3 py-2 bg-neutral-100 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
+      focus:outline-none focus:border-sky-500 focus:bg-white focus:ring-1 focus:ring-sky-500
+      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
+      invalid:border-pink-500 invalid:text-pink-600
+      focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:bg-white "
+                name="email"
+              />
+
+              <span className="mt-2 hidden text-xs text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                Please enter a valid email address
+              </span>
+            </div>
+            <div className="h-24">
+              <label
+                htmlFor="username"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white "
+              >
+                User Name
+              </label>
+              <input
+                type="text"
                 className="mt-1 block w-full px-3 py-2 bg-neutral-100 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:bg-white focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -24,18 +45,21 @@ export default function loginPage() {
       focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:bg-white
     
     "
-                name="email"
+                name="username"
               />
+              <span className="mt-2 hidden text-sm text-red-500 peer-[&:not(:placeholder-shown):not(:focus):invalid]:block">
+                user name is mandatory
+              </span>
             </div>
-            <div className="my-4">
+            <div className="h-24">
               <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white invalid:text-red-500"
+                htmlFor="password"
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white "
               >
-                user name
+                Password
               </label>
               <input
-                type="email"
+                type="password"
                 className="mt-1 block w-full px-3 py-2 bg-neutral-100 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
       focus:outline-none focus:border-sky-500 focus:bg-white focus:ring-1 focus:ring-sky-500
       disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
@@ -43,28 +67,12 @@ export default function loginPage() {
       focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:bg-white
     
     "
-                name="email"
+                name="password"
               />
             </div>
-            <div className="my-4">
-              <label
-                htmlFor="email"
-                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white invalid:text-red-500"
-              >
-                user name
-              </label>
-              <input
-                type="email"
-                className="mt-1 block w-full px-3 py-2 bg-neutral-100 border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400
-      focus:outline-none focus:border-sky-500 focus:bg-white focus:ring-1 focus:ring-sky-500
-      disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none
-      invalid:border-pink-500 invalid:text-pink-600
-      focus:invalid:border-pink-500 focus:invalid:ring-pink-500 invalid:bg-white
-    
-    "
-                name="email"
-              />
-            </div>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 w-full px-4 rounded">
+              Sign Up
+            </button>
           </form>
         </div>
       </LandingWrapper>
