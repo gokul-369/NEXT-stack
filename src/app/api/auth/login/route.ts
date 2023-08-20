177c3f9eb88adbd61761714c985c2e2bfd29829a
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2023-08-20 18:21:21 +0530

    debugging profile issue

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -42,7 +42,7 @@ export async function POST(request: NextRequest) {
     });
     response.cookies.set("token", token, {
       httpOnly: true,
-      secure: true,
+      path: "/",
     });
     return response;
   } catch (error: any) {
