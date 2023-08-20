51d031ce591c2b98d2ce684af723f5e6826a727c
Gokul Rao <64262019+gokul-369@users.noreply.github.com>
2023-08-20 16:57:58 +0530

    cookie policy 2

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -42,7 +42,7 @@ export async function POST(request: NextRequest) {
     });
     response.cookies.set("token", token, {
       httpOnly: true,
-      sameSite: true,
+      secure: true,
     });
     return response;
   } catch (error: any) {
--- src/helpers/getUserById.ts
+++ src/helpers/getUserById.ts
@@ -4,10 +4,9 @@ import jwt from "jsonwebtoken";
 export const getDataFromToken = (request: NextRequest) => {
   try {
     const encToken = request.cookies.get("token")?.value || "";
-    const decToken: any = jwt.verify(
-      encToken,
-      process.env.TOKEN_SECRET!
-    );
+    console.log(encToken);
+
+    const decToken: any = jwt.verify(encToken, process.env.TOKEN_SECRET!);
     return decToken.id;
   } catch (error: any) {
     throw new Error(error.message);
